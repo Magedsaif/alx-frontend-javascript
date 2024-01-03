62cd965d49107ddb40445a6bd7fd8e9298a57dfa
Maged Saif <magedsaif21@gmail.com>
2024-01-03 16:06:17 +0200

    edit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoValue, userValue]) => {
-      const photo = Object.values(photoValue)[1];
-      const user = Object.values(userValue).join(' ');
-      console.log(photo, user);
+    //   const photo = Object.values(photoValue)[1];
+    //   const user = Object.values(userValue).join(' ');
+      console.log(`${photoValue.body} ${userValue.firstName} ${userValue.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
