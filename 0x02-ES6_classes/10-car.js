0f2f67334434ddf6240731eefcdfb816cc6ff01a
Maged Saif <magedsaif21@gmail.com>
2024-01-05 21:16:30 +0200

    task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const ClonedCar = this.constructor[Symbol.clonedCar];
+
+    return new ClonedCar();
+  }
+}
