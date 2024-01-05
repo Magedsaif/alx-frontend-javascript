da05baf02c017a1d2540ad1dce6196a45cb6dfce
Maged Saif <magedsaif21@gmail.com>
2024-01-05 19:47:00 +0200

    edit 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -21,7 +21,9 @@ export default class Pricing {
   }
 
   set currency(newCurrency) {
-    this._currency = newCurrency;
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    }
   }
 
   // instance method
