3a2f26217ae2925bc1dd5943da4039ab7fcafa7c
magedsaif <magedsaif21@gmail.com>
2024-01-08 16:24:52 +0200

    task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-export default function getStudentIdsSum(students){
-return students.reduce((added,obj) => {obj.id + added;
-}
-)
+export default function getStudentIdsSum(students) {
+  return students.reduce((added, obj) => added + obj.id, 0);
 }
